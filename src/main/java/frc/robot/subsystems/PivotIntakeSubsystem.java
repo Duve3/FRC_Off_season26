8f3e755430352ec63eebb1a8760591e4c48a300c
Duve3 <Duv3tabest@gmail.com>
2025-10-07 19:15:56 -0700

    Testing new pivot values

--- src/main/java/frc/robot/subsystems/PivotIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/PivotIntakeSubsystem.java
@@ -1,5 +1,11 @@
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+import static edu.wpi.first.units.Units.RotationsPerSecondPerSecond;
+import static edu.wpi.first.units.Units.Second;
+
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.controls.PositionVoltage;
@@ -10,6 +16,7 @@ import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
 import com.ctre.phoenix6.hardware.CANrange;
 
+import edu.wpi.first.units.Units.*;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -54,21 +61,35 @@ public class PivotIntakeSubsystem extends SubsystemBase {
         config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
         config.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
         
-        // PID Configuration for position control
-        config.Slot0.kP = PivotIntakeConstants.PIVOT_KP;
-        config.Slot0.kI = PivotIntakeConstants.PIVOT_KI;
-        config.Slot0.kD = PivotIntakeConstants.PIVOT_KD;
-        //config.Slot0.kA = 0; // ensure they are reset to 0
-        //config.Slot0.kS = 0; // ensure they are reset to 0
-        //config.Slot0.kV = 0; // ensure they are reset to 0
-        config.Slot0.kG = PivotIntakeConstants.PIVOT_KG;  // Gravity compensation
-        //config.Slot0.kA = PivotIntakeConstants.PIVOT_KA;
-        config.Slot0.GravityType = com.ctre.phoenix6.signals.GravityTypeValue.Arm_Cosine; // For pivoting arms
+        // // PID Configuration for position control
+        // config.Slot0.kP = PivotIntakeConstants.PIVOT_KP;
+        // config.Slot0.kI = PivotIntakeConstants.PIVOT_KI;
+        // config.Slot0.kD = PivotIntakeConstants.PIVOT_KD;
+        // //config.Slot0.kA = 0; // ensure they are reset to 0
+        // //config.Slot0.kS = 0; // ensure they are reset to 0
+        // //config.Slot0.kV = 0; // ensure they are reset to 0
+        // config.Slot0.kG = PivotIntakeConstants.PIVOT_KG;  // Gravity compensation
+        // //config.Slot0.kA = PivotIntakeConstants.PIVOT_KA;
+        // config.Slot0.GravityType = com.ctre.phoenix6.signals.GravityTypeValue.Arm_Cosine; // For pivoting arms
         
-        // shrug i hop eit works
-        config.MotionMagic.MotionMagicCruiseVelocity = 125;
-        config.MotionMagic.MotionMagicAcceleration = 250;
-        config.MotionMagic.MotionMagicJerk = 0; // resetting the value
+        // // shrug i hop eit works
+        // config.MotionMagic.MotionMagicCruiseVelocity = 125;
+        // config.MotionMagic.MotionMagicAcceleration = 250;
+        // config.MotionMagic.MotionMagicJerk = 0; // resetting the value
+
+        MotionMagicConfigs mm = config.MotionMagic;
+        mm.withMotionMagicCruiseVelocity(RotationsPerSecond.of(5)) // 5 (mechanism) rotations per second cruise
+        .withMotionMagicAcceleration(RotationsPerSecondPerSecond.of(10)) // Take approximately 0.5 seconds to reach max vel
+        // Take approximately 0.1 seconds to reach max accel 
+        .withMotionMagicJerk(RotationsPerSecondPerSecond.per(Second).of(100));
+
+        Slot0Configs slot0 = config.Slot0;
+        slot0.kS = 0.25; // Add 0.25 V output to overcome static friction
+        slot0.kV = 0.12; // A velocity target of 1 rps results in 0.12 V output
+        slot0.kA = 0.01; // An acceleration of 1 rps/s requires 0.01 V output
+        slot0.kP = 60; // A position error of 0.2 rotations results in 12 V output
+        slot0.kI = 0; // No output for integrated error
+        slot0.kD = 0.5; // A velocity error of 1 rps results in 0.5 V output
         
         // Use the remote CANcoder for absolute position feedback
         config.Feedback.FeedbackRemoteSensorID = PivotIntakeConstants.PIVOT_ENCODER_ID;
